1b57e6ceaca16947feea607c57fd837343e5112b
Husnain192 <husnainabbas065@gmail.com>
2025-06-10 19:54:37 +0500

    Updates

--- netflix-clone-app/next.config.js
+++ netflix-clone-app/next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
 module.exports = {
   images: {
-    domains: ['img.youtube.com'],
+    domains: ['img.youtube.com', 'cdn.pixabay.com'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = {
+        ...config.externals,
+        bcrypt: 'bcrypt',
+        'node-pre-gyp': 'node-pre-gyp',
+        'detect-libc': 'detect-libc',
+        'https-proxy-agent': 'https-proxy-agent',
+      };
+    }
+    return config;
   },
 };
